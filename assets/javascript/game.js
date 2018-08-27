18716bdcc78fbe6c8cadb2756fd796803e928f55
Kevin Do <do.kevin.vo@gmail.com>
2018-08-26 20:23:33 -0700

    3rd commit: No random target counter and no restart

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,52 @@
-$(document).ready.ready(function(){
+$(document).ready(function(){
 
     // Global Variables
     //==============================================================================================================
     
+    var currentScoreCounter = 0;
+    var targetScoreCounter = 120; // Establish target score the player would need to get.
+    var winCounter = 0;
+    var lossCounter = 0;
     
+    // Setting up random generator to trigger lose condition or win condition
+    var randomCrystalNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    var increasingCurrentScore = randomCrystalNum[Math.round(Math.random() * 12) + 1];
+
 
     // Functions
     //==============================================================================================================
     
     function gameStart() {
 
-        $(".crystal-image").on("click", function() {
+        // Show target score to see if it's working
+        $("#targetScoreCounter").text(targetScoreCounter);
 
-            alert("You picked a crystal.");
+        $(".crystal-image").on("click", function() {
+            
+            // Player's score goes up for every time the crystal's selected.
+            currentScoreCounter += increasingCurrentScore;
+
+            // Register crystal selection & current score
+            document.getElementById("currentScoreCounter").innerHTML = currentScoreCounter;
+            console.log("Player picked a crystal. Current score: " + currentScoreCounter);
+
+            // Win condition
+            if (currentScoreCounter === targetScoreCounter) {
+                winCounter++;
+                document.getElementById("winCounter").innerHTML = winCounter;
+                alert("You won this round.");
+                console.log("You won this round.");
+            }
+            
+            // Lose condition
+            else if (currentScoreCounter > targetScoreCounter) {
+                lossCounter++;
+                document.getElementById("lossCounter").innerHTML = lossCounter;
+                alert("You lose this round.");
+                console.log("You lose this round.");
+            }
+
+            
         
         });
     }
@@ -20,6 +54,7 @@ $(document).ready.ready(function(){
     // Main Process
     //==============================================================================================================
 
+    // Initiate game
     gameStart();
 
 
